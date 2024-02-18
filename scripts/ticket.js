b6d3d8139563b63760ddf51d0c57d67ae32610ae
mehedi <shahebd25@gmail.com>
2024-02-18 17:47:41 +0600

    limited seats selected added

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,27 +1,57 @@
  
  const allBtn = document.getElementsByClassName('seat');
-    const arraySeat = [];
+    
  for (const btn of allBtn) {
     // console.log(btn);
     btn.addEventListener('click', function(e){
-        console.log('shahi click');
-        // console.log(btn.innerText)
+
+        const selectSeatNumber = getTextElementValue('seat-select');
+        console.log(selectSeatNumber);
+
+        if(selectSeatNumber >= 4){
+            return alert();
+        }
         const btnId = btn.innerText;
         setBackgroundColorById(btnId);
-        arraySeat.push(btnId);
-        console.log(arraySeat);
+        
+        // append
+        const seatClassPrice = document.getElementById('seat-class-price');
 
-        // seats left
-        const currentSeats = getTextElementValue('seats-left');
-        // console.log(currentSeats);
-        const updateSeats = currentSeats - 1;
-        console.log(updateSeats);
-        setTextElementValue('seats-left', updateSeats);
+        const tr = document.createElement('tr')
 
-        // seat select
-        const currentSelect = getTextElementValue('seat-select');
-        const updateSelect = currentSelect + 1;
-        setTextElementValue('seat-select', updateSelect);
+        const td1 = document.createElement('td');
+        td1.innerText = btnId;
+
+        const td2 = document.createElement('td');
+        td2.innerText = "Economoy";
+
+        const td3 = document.createElement('td');
+        td3.innerText = 550;
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        seatClassPrice.append(tr);
+
+         // seats left
+         const currentSeats = getTextElementValue('seats-left');
+         // console.log(currentSeats);
+         const updateSeats = currentSeats - 1;
+        //  console.log(updateSeats);
+         setTextElementValue('seats-left', updateSeats);
+ 
+         // seat select
+         const currentSelect = getTextElementValue('seat-select');
+         const updateSelect = currentSelect + 1;
+         setTextElementValue('seat-select', updateSelect);
+
+        // array check
+        
+              
+
+       
+
     })
     
  }
